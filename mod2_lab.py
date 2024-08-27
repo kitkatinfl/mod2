6bd6d637c438ef6ef223ed146c1bdd51f7f589fc
Kathleen Hambrick <mhambrick3@ivytech.edu>
2024-08-27 19:45:03 -0400

    new lab

---
+++ mod2_lab.py
@@ -0,0 +1,21 @@
+# Kathleen Hambrick
+# Program mod2_lab.py in Ktest
+# Check students GPA for honor role or Dean's list levels
+
+last_name=''
+first_name=''
+gpa=''
+
+while last_name!='zzz':
+    last_name = input('Last Name : ')
+
+    #end program when zzz entered
+    if last_name=='zzz':
+        continue
+
+    first_name = input('First Name : ')
+    gpa = float(input('GPA :'))
+
+    #check gpa for honor roll or deans list
+
+
